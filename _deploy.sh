3e0264a8e078956e002b60b5dcb0f1378ffedcc9
Jeff Hanson <jeffrey.hanson@uqconnect.edu.au>
2018-03-28 09:53:39 +1000

    used bot for deployment

--- _deploy.sh
+++ _deploy.sh
@@ -5,8 +5,8 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "jeffrey.hanson@uqconnect.edu.au"
-git config --global user.name "Jeffrey Hanson"
+git config --global user.email "jeff.o.hanson+bot@gmail.com"
+git config --global user.name "bird-team-bot"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
